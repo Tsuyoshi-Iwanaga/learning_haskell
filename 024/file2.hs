2cda301818e57a6459b6fb29dc16d2b80a6993be
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-02-26 00:19:11 +0900

    move lesson24

--- 024/file2.hs
+++ 024/file2.hs
@@ -1,15 +1,6 @@
 import System.Environment
 import System.IO
 
-main :: IO ()
-main = do
-  args <- getArgs
-  let fileName = head args
-  input <- readFile fileName
-  let summary = (countsText . getCounts) input
-  appendFile "stats.dat" (mconcat [fileName, " ", summary, "\n"])
-  putStrLn summary
-
 getCounts :: String -> (Int, Int, Int)
 getCounts input = (charCount, wordCount, lineCount)
   where
@@ -19,3 +10,24 @@ getCounts input = (charCount, wordCount, lineCount)
 
 countsText :: (Int, Int, Int) -> String
 countsText (cc, wc, lc) = unwords ["chars: ", show cc, " words: ", show wc, " lines: ", show lc]
+
+-- (countsText . getCounts) "this is\n some text"
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let fileName = head args
+  file <- openFile fileName ReadMode
+  input <- hGetContents file
+  let summary = (countsText . getCounts) input
+  putStrLn summary
+  hClose file
+  appendFile "stats.dat" (mconcat [fileName, " ", summary, "\n"])
+
+-- main = do
+--   args <- getArgs
+--   let fileName = head args
+--   input <- readFile fileName
+--   let summary = (countsText . getCounts) input
+--   appendFile "stats.dat" (mconcat [fileName, " ", summary, "\n"])
+--   putStrLn summary
