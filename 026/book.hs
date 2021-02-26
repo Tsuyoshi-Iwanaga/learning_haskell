3debbdaf67bb45767086327791c48da123be4a77
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-02-27 00:56:58 +0900

    lesson25-3

--- 026/book.hs
+++ 026/book.hs
@@ -52,11 +52,11 @@ type MarcLeaderRaw = B.ByteString
 leaderLength :: Int
 leaderLength = 24 -- リーダーは先頭24バイト
 
-rowToInt :: B.ByteString -> Int
-rowToInt = (read . T.unpack . E.decodeUtf8)
+rawToInt :: B.ByteString -> Int
+rawToInt = (read . T.unpack . E.decodeUtf8)
 
 getRecordLength :: MarcLeaderRaw -> Int
-getRecordLength leader = rowToInt (B.take 5 leader) -- 先頭5バイトがレコードの長さを表す
+getRecordLength leader = rawToInt (B.take 5 leader) -- 先頭5バイトがレコードの長さを表す
 
 getLeader :: MarcRecordRaw -> MarcLeaderRaw
 getLeader record = B.take leaderLength record
@@ -71,8 +71,131 @@ allRecords marcStream = if marcStream == B.empty
   else next : allRecords rest
   where (next, rest) = nextAndRest marcStream
 
+-- main :: IO ()
+-- main = do
+--   marcData <- B.readFile "sample.mrc"
+--   let marcRecords = allRecords marcData
+--   print (length marcRecords)
+
+-- ディレクトリを読み取る
+type MarcDirectoryRaw = B.ByteString
+
+getBaseAddress :: MarcLeaderRaw -> Int
+getBaseAddress leader = rawToInt (B.take 5 remainder)
+  where remainder = B.drop 12 leader -- リーダーの12文字目から16番目のバイト(5バイト)がベースアドレスの情報
+
+getDirectoryLength :: MarcLeaderRaw -> Int
+getDirectoryLength leader = getBaseAddress leader - (leaderLength + 1)
+
+getDirectory :: MarcRecordRaw -> MarcDirectoryRaw
+getDirectory record = B.take directoryLength afterLeader
+  where
+    directoryLength = getDirectoryLength record
+    afterLeader = B.drop leaderLength record
+
+-- ディレクトリをエントリに分割する
+type MarcDirectoryEntryRaw = B.ByteString
+
+dirEntryLength :: Int
+dirEntryLength = 12 -- 長さは12で固定
+
+splitDirectory :: MarcDirectoryRaw -> [MarcDirectoryEntryRaw]
+splitDirectory directory = if directory == B.empty
+  then []
+  else nextEntry : splitDirectory restEntries
+  where (nextEntry, restEntries) = B.splitAt dirEntryLength directory
+
+-- ディレクトリのエントリを処理し、MARCレコードのフィールドを調べる
+data FieldMetadata = FieldMetadata { tag :: T.Text, fieldLength :: Int, fieldStart :: Int} deriving Show
+
+makeFieldMetadata :: MarcDirectoryEntryRaw -> FieldMetadata
+makeFieldMetadata entry = FieldMetadata textTag theLength theStart
+  where
+    (theTag, rest) = B.splitAt 3 entry
+    textTag = E.decodeUtf8 theTag
+    (rawLength, rawStart) = B.splitAt 4 rest
+    theLength = rawToInt rawLength
+    theStart = rawToInt rawStart
+
+getFieldMetadata :: [MarcDirectoryEntryRaw] -> [FieldMetadata]
+getFieldMetadata rawEntries = map makeFieldMetadata rawEntries
+
+type FieldText = T.Text
+
+getTextField :: MarcRecordRaw -> FieldMetadata -> FieldText
+getTextField record fieldMetadata = E.decodeUtf8 byteStringValue
+  where
+    recordLength = getRecordLength record
+    baseAddress = getBaseAddress record
+    baseRecord = B.drop baseAddress record
+    baseAtEntry = B.drop (fieldStart fieldMetadata) baseRecord
+    byteStringValue = B.take (fieldLength fieldMetadata) baseAtEntry
+
+fieldDelimiter :: Char
+fieldDelimiter = toEnum 31
+
+titleTag :: T.Text
+titleTag = "245"
+
+titleSubfield :: Char
+titleSubfield = 'a'
+
+authorTag :: T.Text
+authorTag = "100"
+
+authorSubfield :: Char
+authorSubfield = 'a'
+
+
+lookupFieldMetadata :: T.Text -> MarcRecordRaw -> Maybe FieldMetadata
+lookupFieldMetadata aTag record = if length results < 1
+  then Nothing
+  else Just (head results)
+  where
+    metadata = (getFieldMetadata . splitDirectory . getDirectory) record
+    results = filter ((== aTag) . tag) metadata
+
+lookupSubfield :: (Maybe FieldMetadata) -> Char -> MarcRecordRaw -> Maybe T.Text
+lookupSubfield Nothing subfield record = Nothing
+lookupSubfield (Just fieldMetadata) subfield record = if results == []
+  then Nothing
+  else Just ((T.drop 1 . head) results)
+  where
+    rawField = getTextField record fieldMetadata
+    subfields = T.split (== fieldDelimiter) rawField
+    results = filter ((== subfield) . T.head) subfields
+
+lookupValue :: T.Text -> Char -> MarcRecordRaw -> Maybe T.Text
+lookupValue aTag subfield record = lookupSubfield entryMetadata subfield record
+  where entryMetadata = lookupFieldMetadata aTag record
+
+lookupTitle :: MarcRecordRaw -> Maybe Title
+lookupTitle = lookupValue titleTag titleSubfield
+
+lookupAuthor :: MarcRecordRaw -> Maybe Author
+lookupAuthor = lookupValue authorTag authorSubfield
+
+-- 1つにまとめる
+marcToPairs :: B.ByteString -> [(Maybe Title, Maybe Author)]
+marcToPairs marcStream = zip titles authors
+  where
+    records = allRecords marcStream
+    titles = map lookupTitle records
+    authors = map lookupAuthor records
+
+pairToBooks :: [(Maybe Title, Maybe Author)] -> [Book]
+pairToBooks pairs = map (\(title, author) -> Book {
+  title = fromJust title,
+  author = fromJust author
+}) justPairs
+  where justPairs = filter (\(title, author) -> isJust title && isJust author) pairs
+
+processRecords :: Int -> B.ByteString -> Html
+processRecords n = booksToHtml . pairToBooks . (take n) . marcToPairs
+
+--IO
 main :: IO ()
 main = do
   marcData <- B.readFile "sample.mrc"
-  let marcRecords = allRecords marcData
-  print (length marcRecords)
+  let processed = processRecords 500 marcData
+  TIO.writeFile "books.html" processed
